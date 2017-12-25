9d386bfca99c9dfcfde4c9daec3fff948511834a
Rikmen <Rikmenv@yandex.ru>
2017-12-25 20:01:54 +0300

    Вторая активность прописана

--- app/src/main/java/com/example/vova/myappforxml/MainActivity.java
+++ app/src/main/java/com/example/vova/myappforxml/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.vova.myappforxml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_xapo_bitcoin_wallet_vault);
-    }
+        setContentView(R.layout.activity_bitcoin_wallet_coinbase);
+        }
 }
